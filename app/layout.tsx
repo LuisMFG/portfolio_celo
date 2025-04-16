3b8cef3880eb02e31a9a4ff04d4e9cb8377d50cd
Luis Marcelo <lmfg10@gmail.com>
2025-04-16 12:22:06 -0400

    troquei a fonte pra sora com pacote do googlefonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Sora } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 
-const inter = Inter({ subsets: ['latin'] });
+const sora = Sora({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'], 
+})
 
 export const metadata: Metadata = {
   title: 'Luis Marcelo - Desenvolvedor Full Stack',
@@ -18,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={sora.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
